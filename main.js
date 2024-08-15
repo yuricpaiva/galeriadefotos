78ac7c46cac243edca44e31588343f3237fbbea2
yuri.paiva <yuricavalcantepaiva@gmail.com>
2024-08-15 10:47:31 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+     $('header button').click(function() {
+        $('form').slideDown();
+
+    })
+
+    $('#botao-cancelar').click(function () {
+        $('form').slideUp()
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault()
+        const enderecoNovaImagem = $('#endereco-imagem-nova').val()
+        const novoItem = $('<li style="display: none;"></li>')
+        $(`<img src="${enderecoNovaImagem}"/>`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link"> 
+                <a href="${enderecoNovaImagem}" target="_blank" title="ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(500);
+        $('#endereco-imagem-nova').val('');
+    })
+
+})
+
+
